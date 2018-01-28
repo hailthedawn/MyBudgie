b434b3328b298fc4d60fcee0733c0856792b58bb
Akash Eldo <akasheldo@hotmail.com>
2018-01-27 20:09:12 -0500

    Created Day and Entry Activities

--- app/src/main/java/com/example/varni/mybudgie/MainActivity.java
+++ app/src/main/java/com/example/varni/mybudgie/MainActivity.java
@@ -1,21 +1,152 @@
 package com.example.varni.mybudgie;
 
-import java.util.Calendar;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Currency;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        getSupportActionBar().setTitle("Jan. 27th Summary");
 
-//        One way to use the calendar widget is putting it in the xml file is shown in main.xml
-//        setContentView(R.layout.main);
-        MonthView mv = new MonthView(this);
-        setContentView(mv);
-//        Calendar cal = Calendar.getInstance();
-//        cal.set(2012, Calendar.DECEMBER,12);
-//        mv.GoToDate(cal.getTime());
+        TextView totalText = (TextView)findViewById(R.id.total_amt);
+        ListView expensesView = (ListView)findViewById(R.id.expenses_list);
+        ListView earningsView = (ListView)findViewById(R.id.earnings_list);
+
+        ArrayList<BigDecimal> expenses = getExpenseValue();
+        ArrayList<BigDecimal> earnings = getEarningValue();
+        BigDecimal expenseTotal = calculateTotal(expenses).setScale(2, RoundingMode.HALF_UP);
+        BigDecimal earningTotal = calculateTotal(earnings).setScale(2, RoundingMode.HALF_UP);
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(getResources().getConfiguration().locale);
+        ((TextView)findViewById(R.id.expenses_total)).setText(currencyFormatter.format(expenseTotal));
+        ((TextView)findViewById(R.id.earnings_total)).setText(currencyFormatter.format(earningTotal));
+
+        double d = earningTotal.subtract(expenseTotal).doubleValue();
+        String value = currencyFormatter.format(d);
+        if(d >=0){
+            totalText.setText(value);
+            totalText.setTextColor(getResources().getColor(R.color.green));
+        }else{
+            totalText.setText(value);
+            totalText.setTextColor(getResources().getColor(R.color.red));
+        }
+
+
+        EntryAdapter expensesAdapter = new EntryAdapter(getExpenseNames(),expenses);
+        EntryAdapter earningsAdapter = new EntryAdapter(getEarningNames(),earnings);
+        expensesView.setAdapter(expensesAdapter);
+        earningsView.setAdapter(earningsAdapter);
     }
+
+    private class EntryAdapter extends BaseAdapter {
+
+        private ArrayList<String> names;
+        private ArrayList<BigDecimal> values;
+        private final int RESOURCE_ID = R.layout.item_conjugation;
+
+        public EntryAdapter(ArrayList<String> names, ArrayList<BigDecimal> values) {
+            this.names = names;
+            this.values = values;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                view = LayoutInflater.from(viewGroup.getContext()).inflate(RESOURCE_ID, viewGroup, false);
+            }
+            TextView nameView = (TextView)view.findViewById(R.id.conjFormal);
+            TextView amtView = (TextView) view.findViewById(R.id.conjText);
+            nameView.setText(names.get(i));
+
+            NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(getResources().getConfiguration().locale);
+            String value = currencyFormatter.format(values.get(i));
+            amtView.setText(value);
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(MainActivity.this,EntryActivity.class);
+                    startActivity(i);
+                }
+            });
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return names.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return names.get(i);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+    }
+
+    private ArrayList<String> getExpenseNames(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Expense 1");
+        list.add("Expense 2");
+        list.add("Expense 3");
+        return list;
+    }
+
+    private ArrayList<BigDecimal> getExpenseValue(){
+        ArrayList<BigDecimal> list = new ArrayList<>();
+        list.add(new BigDecimal(13.37));
+        list.add(new BigDecimal(150));
+        list.add(new BigDecimal(0.56));
+        return list;
+    }
+
+    private ArrayList<String> getEarningNames(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Earning 1");
+        list.add("Earning 2");
+        return list;
+    }
+
+    private ArrayList<BigDecimal> getEarningValue(){
+        ArrayList<BigDecimal> list = new ArrayList<>();
+        list.add(new BigDecimal(13.37));
+        list.add(new BigDecimal(152));
+        return list;
+    }
+
+
+    private BigDecimal calculateTotal(ArrayList<BigDecimal> expenses){
+        BigDecimal total = new BigDecimal(0);
+        for(BigDecimal d: expenses){
+            total = total.add(d);
+        }
+        return total;
+    }
+
 }
